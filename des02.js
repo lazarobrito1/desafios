0503b927de043ea70ba1db5d24d571deef531884
lázaro <lazabrito30p@gmail.com>
2023-11-24 15:41:16 -0300

    Desafios

---
+++ des02.js
@@ -0,0 +1,5 @@
+
+let clicar =()=>{
+  var a = window.prompt('Digite um número')
+  window.alert(`o valor antecedente é ${a - 1}
+  e o subsequente é ${++a}`)}
---
+++ exx001.js
@@ -0,0 +1,16 @@
+let a = document.getElementById('bt1')
+let b = document.getElementById('bt2')
+let c = document.getElementById('bt3')
+
+
+function clicar () {
+  window.alert ('vc clicou no botao 1')
+  
+}
+ function clicard (){
+   window.alert ('vc clicou no botão 2')
+   
+ }
+ function clicardi(){
+   window.alert('vc clicou no botao 3')
+ }
